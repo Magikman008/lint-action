9227c4233324a1f92e839e60dbff28d571297f86
Andrew Dubov <40332480+Magikman008@users.noreply.github.com>
2024-08-06 21:57:16 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -7478,7 +7478,9 @@ class ESLint {
 		const { stdout: version } = run(`${commandPrefix} eslint -v`, { dir });
 		const isV9 = version.startsWith("v9.");
 		const extensionsArg =
-			extensions.length > 0 && extensions[0] !== "" ? `--ext ${extensions.map((ext) => `.${ext}`).join(",")}` : "";
+		    extensions.length > 0 && extensions[0] !== "" 
+		        ? `--ext ${extensions.map((ext) => `.${ext}`).join(",")}` 
+		        : "";
 		
 		const fixArg = fix ? "--fix" : "";
 
